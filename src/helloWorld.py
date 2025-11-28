bd848bdd31c7089fb3f2af9ab6e31e78c21580c4
ilemonae <mehllodie@gmail.com>
2025-11-28 17:35:22 -0600

    basic screen setup

--- src/helloWorld.py
+++ src/helloWorld.py
@@ -1,1 +1,19 @@
-print("helloWorld")
+import sys, pygame
+import colours
+pygame.init()
+size = width, height = 1000, 800
+flags = pygame.FULLSCREEN | pygame.SCALED
+screen = pygame.display.set_mode(size, flags)
+pygame.display.set_caption("hello world.")
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            pygame.quit()
+    screen.fill(colours.black)
+    pygame.draw.circle(screen, colours.white, (0,0), 30)
+    pygame.draw.circle(screen, colours.white, (1000,0), 30)
+    pygame.draw.circle(screen, colours.white, (0,800), 30)
+    pygame.draw.circle(screen, colours.white, (1000,800), 30)
+    pygame.display.flip()
